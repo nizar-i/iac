7dc855c2ffa7cfb552d586097c7b2d4693213ebe
NizarAlsaeed <nizar.ayman@gmail.com>
2024-01-24 23:58:13 +0200

    vars

--- variables.tf
+++ variables.tf
@@ -1,9 +1,10 @@
-
 variable "cluster_name" {
   type        = string
+  default = "poc"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-2"
 }
