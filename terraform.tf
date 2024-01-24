08f8cf8c6cfcc8dc16ee0758dfe8943fed3ce0ef
NizarAlsaeed <nizar.ayman@gmail.com>
2024-01-24 23:01:31 +0200

    fix {

--- terraform.tf
+++ terraform.tf
@@ -5,14 +5,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.32.0"
     }
+  }
 
   backend "s3" {
     bucket = "terraform"
     key    = "state/terraform.tfstate"
     region = "us-east-2"
   }
-
-}
 
   required_version = "~> 1.3"
 }
